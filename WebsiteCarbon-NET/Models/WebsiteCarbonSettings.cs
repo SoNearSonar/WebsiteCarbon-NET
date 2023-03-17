7a91c0f08092041c9526105993209e11c0a3902b
SoNearSonar <sonearsonar@gmail.com>
2023-03-16 22:36:30 -0400

    Remove un-used class
    /site doesn't have params for bytes or green status and /data doesn't have params for url

--- WebsiteCarbon-NET/Models/WebsiteCarbonSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebsiteCarbon_NET.Models
-{
-    public class WebsiteCarbonSettings
-    {
-        public string Url { get; set; }
-        public uint Bytes { get; set; }
-        public uint Green { get; set; }
-    }
-}
